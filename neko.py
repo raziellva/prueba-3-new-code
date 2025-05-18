fd5ecde5e80e5e5c197287d54e8f5c401e69f7c5
raziellva <razielleyvaperez4338@gmail.com>
2025-05-17 20:18:44 -0400

    neko.py

--- neko.py
+++ neko.py
@@ -35,9 +35,9 @@ Me tomo mas tiempo de lo qué pensaba cortar este código, creo que es falta de 
 
 video_settings = {
     'resolution': '854x480',
-    'crf': '35',
-    'audio_bitrate': '60k',
-    'fps': '15',
+    'crf': '25',
+    'audio_bitrate': '150k',
+    'fps': '30',
     'preset': 'veryfast',
     'codec': 'libx264'
 }
@@ -118,11 +118,11 @@ async def add_user(client, message):
     try:
         user_id = int(message.text.split("/adduser")[1])
         if user_id in users:
-            await message.reply_text(f"El ID:  \n`{user_id}`\nya esta en la lista de usuarios permitidos.") 
+            await message.reply_text(f"✅El ID:  \n`{user_id}`\nya esta en la lista de usuarios permitidos.") 
             return
             
         users.append(user_id)
-        await message.reply_text(f"Usuario con ID:\n`{user_id}`\nHa sido añadido.")
+        await message.reply_text(f"✅Usuario con ID:\n`{user_id}`\nHa sido añadido.")
     except (IndexError, ValueError):
         await message.reply_text(f"💢 Formato del comando incorrecto\n /adduser + ID.")
         
@@ -134,16 +134,17 @@ async def remove_user(client, message):
         user_id = int(message.text.split("/banuser")[1]) 
         if user_id in users:
             users.remove(user_id)
-            await message.reply_text(f"💬 Usuario con ID:\n`{user_id}`\nHa sido eliminado de la lista ")
+            await message.reply_text(f"❌Usuario con ID:\n`{user_id}`\nHa sido eliminado de la lista ")
            
         else:
-            await message.reply_text(f"💬 El usuario:  \n`{user_id}`\nno se encuentra en la lista.")
+            await message.reply_text(f"❌El usuario:  \n`{user_id}`\nno se encuentra en la lista.")
     except (IndexError, ValueError):
         await message.reply_text("💢 Formato de comando incorrecto \n /banuser + ID.")
         # Añadir el ID del usuario a la lista temp_users si no está ya añadido
         if user_id not in temp_users:
             temp_users.append(user_id)
-            allowed_users.append(user_id)  # Añadir también a allowed_users
+            allowed_users.append(user_id)  
+        # Añadir también a allowed_users
             message.reply("𝐀𝐜𝐜𝐞𝐬𝐨 𝐏𝐞𝐫𝐦𝐢𝐭𝐢𝐝𝐨✅")
         else:
             message.reply("Ya estás en la lista de acceso temporal.")
